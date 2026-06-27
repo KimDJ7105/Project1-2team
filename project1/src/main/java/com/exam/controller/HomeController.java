f6b4e39cd131bff4ab38af17af94ebd97b7da49a
Kim dong jae <rlaehdwo1225@naver.com>
2026-06-27 17:13:53 +0900

    feat: home 페이지 기능 구현
    home html을 기반으로 상의된 기능들이 작동하도록 구현

--- project1/src/main/java/com/exam/controller/HomeController.java
+++ project1/src/main/java/com/exam/controller/HomeController.java
@@ -1,19 +1,39 @@
 package com.exam.controller;
 
+import com.exam.dto.AccountDTO;
+import com.exam.dto.UserDTO;
+import com.exam.service.AccountService;
 import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.util.List;
+
 @Controller
 @SessionAttributes(value = {"my_login"})
 public class HomeController {
 
+    AccountService accountService;
+    public HomeController(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
 
     @GetMapping("/home")
-    public String  home(){
+    public String  home(HttpSession session, Model model){
+        UserDTO loginUser = (UserDTO) session.getAttribute("myLogin");
+
+        if (loginUser == null) {
+            return "home";
+        }
+
+        List<AccountDTO> accountList = accountService.findAccount(loginUser.getUserId());
+        model.addAttribute("accounts", accountList);
+
         return "home";
     }
 
