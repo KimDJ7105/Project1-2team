a67864a0dc2bbbfeb3c223aa0a988b49937c43f5
Kim dong jae <rlaehdwo1225@naver.com>
2026-06-27 14:25:52 +0900

    feat: signup 기능 구현
    SignupController 구현, UserService의 Signup에 회원가입시에 같은 아이디로 계정 생성을 막기위한 방어 로직 추가
    html 명이나 key 이름은 차후 수정될 가능성이 있음.

--- project1/src/main/java/com/exam/service/UserServiceImpl.java
+++ project1/src/main/java/com/exam/service/UserServiceImpl.java
@@ -17,6 +17,13 @@ public class UserServiceImpl implements UserService {
     @Override
     @Transactional
     public int signup(UserDTO dto) {
+
+        UserDTO existingUser = login(dto.getUsername()); // 로그인용 메서드 재활용
+        if (existingUser != null) {
+            // 입력한 username 값이 이미 존재하는 경우.
+            return 0;
+        }
+
         return memberMapper.signup(dto);
     }
 
