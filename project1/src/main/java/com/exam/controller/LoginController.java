bbe5e9a888a55ffbed6e9b6622175f3f062ae576
Kim dong jae <rlaehdwo1225@naver.com>
2026-06-27 14:05:57 +0900

    feat: LoginController 구현
    로그인 기능을 담당하는 LoginController 구현 완료 차후 html 파일명이나 데이터 key의 경우 팀원과 상의 하에 수정 필요성 있음

--- project1/src/main/java/com/exam/controller/LoginController.java
+++ project1/src/main/java/com/exam/controller/LoginController.java
@@ -1,19 +1,59 @@
 package com.exam.controller;
 
+import com.exam.dto.LoginRequestDTO;
+import com.exam.dto.UserDTO;
+import com.exam.service.UserService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.support.SessionStatus;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.Map;
 
 @Controller
 
 public class LoginController {
 
+    UserService userService;
+    PasswordEncoder passwordEncoder;
+    public LoginController(UserService userService, PasswordEncoder passwordEncoder) {
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @GetMapping("/login")
-    public String loginForm() {
+    public String loginForm(Model model) {
+        model.addAttribute("loginRequestDto", new LoginRequestDTO());
         return "loginForm";
+    }
+
+    @PostMapping("/login")
+    public String loginProcess(@Valid @ModelAttribute("loginRequestDto") LoginRequestDTO loginDto, BindingResult bindingResult, RedirectAttributes rttr, HttpSession session) {
+        if(bindingResult.hasErrors()) {
+            //검증 실패시
+            System.out.println(bindingResult.getAllErrors());
+            return "loginForm";
+        }
+
+        UserDTO DBUser = userService.login(loginDto.getUsername());
+        if(DBUser==null) {
+            // 없는 아이디를 입력한 것.
+            rttr.addFlashAttribute("errorMessage", "아이디 또는 비밀번호가 틀렸습니다.");
+            return "redirect:/login";
+        }
+
+        if(passwordEncoder.matches(loginDto.getPassword(), DBUser.getPassword())) {
+            //로그인 성공
+            DBUser.setPassword(""); // 보안을 위해 비밀번호 날려버리기.
+            session.setAttribute("myLogin", DBUser);
+            return "redirect:/home";
+        }
+
+        //로그인 실패
+        rttr.addFlashAttribute("errorMessage", "아이디 또는 비밀번호가 틀렸습니다.");
+        return "redirect:/login";
     }
 }
