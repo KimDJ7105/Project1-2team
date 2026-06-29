80fb5663ed86af13d20237f731f3c53433bc385a
Kim dong jae <rlaehdwo1225@naver.com>
2026-06-29 16:47:23 +0900

    db: 문제가 생기지 않도록 schema.sql과 data.sql 수정

--- project1/src/main/resources/data.sql
+++ project1/src/main/resources/data.sql
@@ -3,16 +3,16 @@
 --VALUES ('hong123', 'passwd', '홍길동', '1999-11-15');
 
 -- 홍길동 회원당 계좌 3개 개설 (계좌 ID: 1, 2, 3)
-INSERT IGNORE INTO account (user_id, account_number, balance, is_blocked)
-VALUES
-(1, '110-123-000001', 500000, FALSE),  -- 정상 계좌 1
-(1, '110-123-000002', 1200000, FALSE), -- 정상 계좌 2
-(1, '110-123-000003', 10000, TRUE);    -- FDS 차단 시나리오용 계좌 is_blocked : TRUE
+--INSERT IGNORE INTO account (user_id, account_number, balance, is_blocked)
+--VALUES
+--(1, '110-123-000001', 500000, FALSE),  -- 정상 계좌 1
+--(1, '110-123-000002', 1200000, FALSE), -- 정상 계좌 2
+--(1, '110-123-000003', 10000, TRUE);    -- FDS 차단 시나리오용 계좌 is_blocked : TRUE
 
 -- FDS 점수 100점 기록
-INSERT IGNORE INTO trading_history (trade_type, sending_account, receiving_account, amount, message, is_fail, fail_msg)
-VALUES ('송금', 3, 1, 5000000, '의심거래테스트', TRUE, '의심 거래 발견:고액이체');
+--INSERT IGNORE INTO trading_history (trade_type, sending_account, receiving_account, amount, message, is_fail, fail_msg)
+--VALUES ('송금', 3, 1, 5000000, '의심거래테스트', TRUE, '의심 거래 발견:고액이체');
 
 -- 1번 거래 기록(위험 거래)에 대한 FDS 탐지 결과 매핑
-INSERT IGNORE INTO fds (history_id, risk_rank, risk_score, risk_reason)
-VALUES (1, '위험', 100, '단시간 내 비정상적 고액 송금 시도 및 해외 IP 우회 접속 탐지');
+--INSERT IGNORE INTO fds (history_id, risk_rank, risk_score, risk_reason)
+--VALUES (1, '위험', 100, '단시간 내 비정상적 고액 송금 시도 및 해외 IP 우회 접속 탐지');
